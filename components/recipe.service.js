4cee7bfb793e5786fa5333cc311a154a1c2aa8b5
Alex Darmos <alex.darmos@gmail.com>
2019-05-02 19:13:29 -0400

    updated service with promise

--- components/recipe.service.js
+++ components/recipe.service.js
@@ -1,18 +1,51 @@
-function RecipeService($http) {
+function RecipeService($http, $q) {
     const service = this;
     service.APP_KEY = "347156a4086f1d41a3e7afec3aa99d76";
-    service.APP_ID = "130a79d6"
+    service.APP_ID = "130a79d6";
+    service.input = null;
+    service.health = null;
+    service.time = null;
+    service.dish= null;
+    console.log(service.input);
 
 
-    service.fetchRecipes = () => {
-        $http.get(`https://api.edamam.com/search?q=chicken&app_id=${service.APP_ID}&app_key=${service.APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
-            .then((response) => {
-                console.log(response);
+
+    service.fetchRecipes = (searchInput, health, time ) => {
+
+        return $q(function(resolve, reject) {
+            service.input = searchInput;
+
+            $http({
+                url: `https://api.edamam.com/search`,
+                method: `GET`,
+                params: {
+                    q: service.input,
+                    app_id: service.APP_ID,
+                    app_key: service.APP_KEY,
+                    Health: service.health,
+                    DishType: service.dishType,
+                    time: service.time
+                }
             })
+            .then((response) => {
+                    console.log(response);
+                    return [service.input, service.health, service.time,service.dish];
+                    
+                })
+            .catch((error) => {
+                console.log(error);
+            })
+
+
+
+        })
+
+
     }
+
 
 }
 
 angular
     .module("RecipeApp")
-    .service("RecipeService", ["$http", RecipeService]);
+    .service("RecipeService", ["$http", "$q", RecipeService]);
--- components/search-criteria/searchCriteria.js
+++ components/search-criteria/searchCriteria.js
@@ -1,12 +1,41 @@
 function SearchController(RecipeService) {
+    let $ctrl = this;
 
-    RecipeService.fetchRecipes();
+
+    $ctrl.onSearch = (search, time, meal, health) => {
+        RecipeService.fetchRecipes(search, time, meal, health);
+
+    }
 
 }
 
 angular.module("RecipeApp",)
 // .service("RecipeService", ["$http", RecipeService])
 .component("searchCriteria", {
-    template: ``,
+    template: `
+    <input ng-model="userInput" placeholder="Search recipes!"/>
+    <select ng-model="time">
+        <option></option>
+        <option value="30">30 Minutes</option>
+        <option value="45">45 Minutes</option>
+        <option value="60">60 Minutes</option>
+    </select>
+
+    <select ng-model="meal">
+        <option value="breakfast">Breakfast</option>
+        <option value="lunch">Lunch</option>
+        <option value="dinner">Dinner</option>
+    </select>
+
+    <select ng-model="health">
+        <option value="gluten-free">Gluten Free</option>
+        <option value="vegetarian">Vegetarian</option>
+        <option value="vegan">Vegan</option>
+    </select>
+    <button ng-click="$ctrl.onSearch(userInput, time, meal, health)">GO</button>
+    
+
+    
+    `,
     controller: SearchController
 })
