15e0fac01bebe57fbeb4a335bf93ddab454f8c6b
Jess <jessicaotte@ymail.com>
2019-05-04 01:38:37 -0400

    Because of the forEach error in the console, I  hard coded filler recipes in the recipeList.js files  as well as in the recipeDetails.js file. Expanded on the recipeDestails page, however, the template does not recognize the hard coded list from above. Worked up CSS for better visualization.

--- components/recipe-list/recipeList.js
+++ components/recipe-list/recipeList.js
@@ -1,39 +1,68 @@
-function RecipeList(RecipeService, $q,) {
+function RecipeList(RecipeService, $q) {
   const ctrl = this;
   ctrl.fetchRecipes = [];
 
-ctrl.recipeList= [];
-RecipeService.fetchRecipes()
-  .then((response) => {
+  // ctrl.recipeList= [];
+  // RecipeService.fetchRecipes()
+  //   .then((response) => {
 
-    ctrl.recipeList = response;
-    console.log(response);
-  });
+  //     ctrl.recipeList = response;
+  //     console.log(response);
+  //   });
 
 
+  ctrl.recipeList = [
+    {
+      label: "Chicken Quesadilla",
+      img: "img.jpg",
+      calories: '1000',
+      ingredients: "10",
+      servings: "5",
+    },
+    {
+      label: "Turkey Salad",
+      img: "img.jpg",
+      calories: '1000',
+      ingredients: "10",
+      servings: "5",
+    },
+    {
+      label: "Steak Dinner",
+      img: "img.jpg",
+      calories: '1000',
+      ingredients: "10",
+      servings: "5",
+    },
+    {
+      label: "Blueberry Muffins",
+      img: "img.jpg",
+      calories: '1000',
+      ingredients: "10",
+      servings: "5",
+    },
+  ]
 
-  // ctrl.recipesList = [
-  //   {    },
-  // ] 
-   
 }
 
 
-  
-  angular.module('RecipeApp').component('recipeList', {
-    template: `
-    <div class="card-deck" id="container">
+
+angular.module('RecipeApp').component('recipeList', {
+  template: `
+<div class="card-deck text-center" id="container">
     <div ng-repeat="recipe in $ctrl.recipeList" class="card">
-      <img class="card-img-top" ng-img={{recipe.img}} alt="{{recipe.label}}">
+      <img class="card-img-top" src="quesadillas.jpg" alt="{{recipe.label}}">
       <div class="card-body">
         <h5 class="card-title">{{recipe.label}}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">{{recipe.ingredients}}</small></p>
+        <p class="card-text"><small class="text-muted">Number of Ingredients: {{recipe.ingredients}} </br> Calories: {{recipe.calories}}</small></p>
+        <a href="#" class="btn btn-primary">See Recipe</a>
       </div>
     </div>
-
-    `, // or use templateUrl
-    controller: RecipeList,
+    `, 
+  controller: RecipeList,
+  bindings: {
+    recipeService: '&',
+  }
 });
 
 
@@ -41,78 +70,78 @@ RecipeService.fetchRecipes()
 
 
 
-// function RecipeListController(recipeService) {
-//     const ctrl = this;
-//     ctrl.recipesList = [];
-  
-//     ctrl.getList = (search) => {
-//       recipeService.getData(search)
-//         .then((recipes) => {
-//           ctrl.recipesList = []
-//           console.log("it worked!!!!!!s")
-//           console.log(recipes)
+// // function RecipeListController(recipeService) {
+// //     const ctrl = this;
+// //     ctrl.recipesList = [];
 
-//           let listFromApi = recipes;
+// //     ctrl.getList = (search) => {
+// //       recipeService.getData(search)
+// //         .then((recipes) => {
+// //           ctrl.recipesList = []
+// //           console.log("it worked!!!!!!s")
+// //           console.log(recipes)
 
-//           listFromApi.forEach(function (spot, index){
-//             let recipeObj = {
-//               label: spot.recipe.label,
-//               img: spot.recipe.image,
-//               calories: spot.recipe.calories,
-//               ingredients: spot.recipe.ingredients.length,
-//               servings: spot.recipe.yield,
-//               bookmark: spot.bookmarked
-//             }
-//             ctrl.recipesList.push(recipeObj);
-//           })
-//         })
-//         .catch((err) => {
-//           console.log("it didnt work")
-//           console.log(err);
-//         });
-//     }
-  
-    
-  
-  
-//   }
-  
-  
-//   angular
-//     .module('RecipeApp')
-//     .component('recipeList', {
-//       template: `
-//       <search-criteria get-list="$ctrl.getList(search)"></search-criteria>
-//       <div class="cardContainer">
-//           <div ng-repeat="recipe in $ctrl.recipesList" class="fullCard">
-//             <div class="imageCard">
-//               <!-- <div class="favorite">
-//                     <i class="material-icons favoriteIcon">favorite_border</i>
-//                     <i class="material-icons favoriteIcon">favorite</i>
-//                 </div> -->
-//               <img class="foodImage" src="{{recipe.img}}" alt="food">
-//             </div>
-//             <div class="informationCard">
-//               <h2 class="cardDefault cardParams cardHeader cardSpacing">{{recipe.label}}</h2>
-//               <div class="cardStats">
-//                 <p class="cardDefault rightBorder">Calories:
-//                   <span class="cardParams">{{recipe.calories}}</span>
-//                 </p>
-//                 <p class="cardDefault">Servings:
-//                   <span class="cardParams">{{recipe.servings}}</span>
-//                 </p>
-//               </div>
-//               <p class="cardDefault cardSpacing">Ingredients Needed:
-//                 <span class="cardParams">7</span>
-//               </p>
-//             </div>
-//         </div>
-//       </div>
-//     `, // or use templateUrl
-//       controller: RecipeListController,
-//       // bindings: {
-//       //   me: '<',
-//       //   onDelete: '&',
-//       //   onUpdate: '&'
-//       // }
-//     });
+// //           let listFromApi = recipes;
+
+// //           listFromApi.forEach(function (spot, index){
+// //             let recipeObj = {
+// //               label: spot.recipe.label,
+// //               img: spot.recipe.image,
+// //               calories: spot.recipe.calories,
+// //               ingredients: spot.recipe.ingredients.length,
+// //               servings: spot.recipe.yield,
+// //               bookmark: spot.bookmarked
+// //             }
+// //             ctrl.recipesList.push(recipeObj);
+// //           })
+// //         })
+// //         .catch((err) => {
+// //           console.log("it didnt work")
+// //           console.log(err);
+// //         });
+// //     }
+
+
+
+
+// //   }
+
+
+// //   angular
+// //     .module('RecipeApp')
+// //     .component('recipeList', {
+// //       template: `
+// //       <search-criteria get-list="$ctrl.getList(search)"></search-criteria>
+// //       <div class="cardContainer">
+// //           <div ng-repeat="recipe in $ctrl.recipesList" class="fullCard">
+// //             <div class="imageCard">
+// //               <!-- <div class="favorite">
+// //                     <i class="material-icons favoriteIcon">favorite_border</i>
+// //                     <i class="material-icons favoriteIcon">favorite</i>
+// //                 </div> -->
+// //               <img class="foodImage" src="{{recipe.img}}" alt="food">
+// //             </div>
+// //             <div class="informationCard">
+// //               <h2 class="cardDefault cardParams cardHeader cardSpacing">{{recipe.label}}</h2>
+// //               <div class="cardStats">
+// //                 <p class="cardDefault rightBorder">Calories:
+// //                   <span class="cardParams">{{recipe.calories}}</span>
+// //                 </p>
+// //                 <p class="cardDefault">Servings:
+// //                   <span class="cardParams">{{recipe.servings}}</span>
+// //                 </p>
+// //               </div>
+// //               <p class="cardDefault cardSpacing">Ingredients Needed:
+// //                 <span class="cardParams">7</span>
+// //               </p>
+// //             </div>
+// //         </div>
+// //       </div>
+// //     `, // or use templateUrl
+// //       controller: RecipeListController,
+// //       // bindings: {
+// //       //   me: '<',
+// //       //   onDelete: '&',
+// //       //   onUpdate: '&'
+// //       // }
+// //     });
--- components/search-criteria/searchCriteria.js
+++ components/search-criteria/searchCriteria.js
@@ -12,29 +12,30 @@ angular.module("RecipeApp",)
 // .service("RecipeService", ["$http", RecipeService])
 .component("searchCriteria", {
     template: `
-    <recipe-list></recipe-list>
-    <input ng-model="userInput" placeholder="Search recipes!"/>
-    <select ng-model="time">
+    <input ng-model="userInput" placeholder="Search recipes!" id="search-box"/>
+    <div id="filter">FILTER: 
+    <select ng-model="time" class="custom-select" style="width:200px;">
         <option></option>
         <option value="30">30 Minutes</option>
         <option value="45">45 Minutes</option>
         <option value="60">60 Minutes</option>
     </select>
 
-    <select ng-model="meal">
+    <select ng-model="meal" class="custom-select" style="width:200px;">
         <option></option>
         <option value="breakfast">Breakfast</option>
         <option value="lunch">Lunch</option>
         <option value="dinner">Dinner</option>
     </select>
 
-    <select ng-model="health">
+    <select ng-model="health" class="custom-select" style="width:200px;">
         <option></option>
         <option value="gluten-free">Gluten Free</option>
         <option value="vegetarian">Vegetarian</option>
         <option value="vegan">Vegan</option>
     </select>
     <button ng-click="$ctrl.onSearch(userInput, time, meal, health)">GO</button>
+    </div>
     
 
     
